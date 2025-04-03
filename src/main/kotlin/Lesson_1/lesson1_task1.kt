5693d08b42165a4d5ad45d28e4bea4de225fda6e
Anton <new_perspective@mail.ru>
2025-04-03 13:40:19 +0300

    KS-1-1 create variables

---
+++ src/main/kotlin/Lesson_1/lesson1_task1.kt
@@ -0,0 +1,8 @@
+package org.example.Lesson_1
+
+fun main() {
+
+    var quantityOfOrders: Int = 75 // переменная, которую можно перезаписывать
+    val thanksMessage: String = "Thank you for your purchase!" // неизменяемое сообщение для пользователя
+
+}
