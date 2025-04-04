f4d4a7742bd6ce55219f7c0eb10c288af31e77d7
Anton <new_perspective@mail.ru>
2025-04-04 16:28:36 +0300

    KS-2-1 calculate the average

---
+++ src/main/kotlin/Lesson_2/lesson2_task1.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    val student1: Float = 3f
+    val student2: Float = 4f
+    val student3: Float = 3f
+    val student4: Float = 5f
+
+    val average: Float = (student1 + student2 + student3 + student4) / 4 // среднее арифметическое
+
+    println(average) // 3.75
+
+}
