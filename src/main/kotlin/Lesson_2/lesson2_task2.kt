e0cb8883c302b8ba9e9200898f0cb8b5dfc38ee4
Anton <new_perspective@mail.ru>
2025-04-04 19:02:34 +0300

    KS-2-2 comments were deleted

--- src/main/kotlin/Lesson_2/lesson2_task2.kt
+++ src/main/kotlin/Lesson_2/lesson2_task2.kt
@@ -7,14 +7,14 @@ fun main() {
     val trainees: Int = 30
     val salaryTrainees: Int = 20000
 
-    val employeeExpenses: Int = employees * salaryEmployee // расходы на сотрудников
-    val traineeExpenses: Int = trainees * salaryTrainees // расходы на стажеров
-    val totalExpenses: Int = employeeExpenses + traineeExpenses // общие расходы
-    val averageSalary: Int = (employeeExpenses + traineeExpenses) / (employees + trainees) // средняя ЗП
+    val employeeExpenses: Int = employees * salaryEmployee
+    val traineeExpenses: Int = trainees * salaryTrainees
+    val totalExpenses: Int = employeeExpenses + traineeExpenses
+    val averageSalary: Int = (employeeExpenses + traineeExpenses) / (employees + trainees)
 
-    println(employeeExpenses) // 1500000
-    println(traineeExpenses) // 600000
-    println(totalExpenses) // 2100000
-    println(averageSalary) // 26250
+    println(employeeExpenses)
+    println(traineeExpenses)
+    println(totalExpenses)
+    println(averageSalary)
 
 }
