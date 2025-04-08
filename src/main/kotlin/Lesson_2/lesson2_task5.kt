7b3f189606cda2114df77b3747943325aab1dc36
Anton <new_perspective@mail.ru>
2025-04-08 21:52:32 +0300

    KS-2-5 bank calculation corrections

--- src/main/kotlin/Lesson_2/lesson2_task5.kt
+++ src/main/kotlin/Lesson_2/lesson2_task5.kt
@@ -1,14 +1,17 @@
 package org.example.Lesson_2
+import kotlin.math.pow
 
-const val YEAR_RATE: Double = 16.7
+const val YEAR_RATE_PERCENTAGE: Double = 16.7
+const val CONVERT_PERCENTAGE_TO_DECIMAL: Int = 100
+const val SAVE_BASE_LINE: Int = 1
 
 fun main() {
 
     val deposit: Int = 70_000
-    val rate: Double = YEAR_RATE / 100
+    val rate: Double = YEAR_RATE_PERCENTAGE / CONVERT_PERCENTAGE_TO_DECIMAL
     val years: Double = 20.0
 
-    val result = deposit * Math.pow(1 + rate, years)
+    val result = deposit * (SAVE_BASE_LINE + rate).pow(years)
 
     println(String.format("%.3f", result))
 
