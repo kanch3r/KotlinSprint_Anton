2f2355fb8ad5fdc34d73264ace3996250603806c
Anton <new_perspective@mail.ru>
2025-04-08 11:02:25 +0300

    KS-3-4 movements of chess

---
+++ src/main/kotlin/Lesson_3/lesson3_task4.kt
@@ -0,0 +1,17 @@
+package org.example.Lesson_3
+
+fun main() {
+
+    var cellFrom = "E2"
+    var cellTo = "E4"
+    var moveCounter: Int = 1
+
+    println("$cellFrom-$cellTo;$moveCounter")
+
+    cellFrom = "D2"
+    cellTo = "D3"
+    moveCounter++
+
+    println("$cellFrom-$cellTo;$moveCounter")
+
+}
