321ead00e21019e9e3afc2560773894dce1a5f79
Anton <new_perspective@mail.ru>
2025-04-10 19:47:21 +0300

    KS-5-1 checking user. adjustments done

--- src/main/kotlin/Lesson_5/lesson5_task1.kt
+++ src/main/kotlin/Lesson_5/lesson5_task1.kt
@@ -1,11 +1,17 @@
 package org.example.Lesson_5
 
+const val MIN_RANGE: Int = 1
+const val MAX_RANGE: Int = 10
+
 fun main() {
 
-    println("Для входа в приложение решите пример: 2 + 3 = ?")
-    val userInput = readln().toInt()
+    val number1: Int = (MIN_RANGE..MAX_RANGE).random()
+    val number2: Int = (MIN_RANGE..MAX_RANGE).random()
 
-    val result = if (userInput == 5) {
+    println("Для входа в приложение решите пример: $number1 + $number2 = ?")
+    val userInputNumber = readln().toInt()
+
+    val result = if (number1 + number2 == userInputNumber) {
         "Добро пожаловать!"
     } else {
         "Доступ запрещён."
