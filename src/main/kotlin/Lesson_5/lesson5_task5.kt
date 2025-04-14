ae6110de54cff2e1340b482351fdf823e1fa8fa4
Anton <new_perspective@mail.ru>
2025-04-14 15:21:49 +0300

    KS-5-5 code was improved

--- src/main/kotlin/Lesson_5/lesson5_task5.kt
+++ src/main/kotlin/Lesson_5/lesson5_task5.kt
@@ -1,5 +1,6 @@
 package org.example.Lesson_5
 
+const val MATCH_NEEDED: Int = 3
 const val MIN_RANGE: Int = 0
 const val MAX_RANGE: Int = 42
 const val JACKPOT: Int = 3
@@ -9,31 +10,12 @@ const val FAIL: Int = 0
 
 fun main() {
 
-    val randomNumber1: Int = (MIN_RANGE..MAX_RANGE).random()
-    val randomNumber2: Int = (MIN_RANGE..MAX_RANGE).random()
-    val randomNumber3: Int = (MIN_RANGE..MAX_RANGE).random()
+    val randomNumbersList = List<Int>(3) { (MIN_RANGE..MAX_RANGE).random() }
 
-    val randomNumbersList = buildList<Int> {
-        add(randomNumber1)
-        add(randomNumber2)
-        add(randomNumber3)
-    }
+    println("Для победы в лотерее вам нужно угадать $MATCH_NEEDED разных числа от 0 до 42!")
+    println("Введите $MATCH_NEEDED числа через Enter от 0 до 42:")
 
-    println("Для победы в лотерее вам нужно угадать три разных числа от 0 до 42!")
-    println("Введите первое любое число от 0 до 42:")
-    val inputUserNumber1: Int = readln().toInt()
-
-    println("Введите второе любое число от 0 до 42:")
-    val inputUserNumber2: Int = readln().toInt()
-
-    println("Введите третье любое число от 0 до 42:")
-    val inputUserNumber3: Int = readln().toInt()
-
-    val userNumberList = buildList<Int> {
-        add(inputUserNumber1)
-        add(inputUserNumber2)
-        add(inputUserNumber3)
-    }
+    val userNumberList = List<Int>(3) { readln().toInt() }
 
     val result = randomNumbersList.intersect(userNumberList)
 
