9333fa32cd3c1cf65c781fbaba976b689a6dce81
Anton <new_perspective@mail.ru>
2025-04-16 11:29:52 +0300

    KS-6-4 guess the number

---
+++ src/main/kotlin/Lesson_6/lesson6_task4.kt
@@ -0,0 +1,26 @@
+package org.example.Lesson_6
+
+const val LOW_RANGE: Int = 1
+const val HIGH_RANGE: Int = 9
+const val ATTEMPTS: Int = 5
+
+fun main() {
+
+    println("Угадай число от $LOW_RANGE до $HIGH_RANGE. У тебя $ATTEMPTS попыток")
+    val number = (LOW_RANGE..HIGH_RANGE).random()
+
+    var counter = ATTEMPTS
+
+    while (counter > 0) {
+        val userInputDigit = readln().toInt()
+
+        if (userInputDigit == number) {
+            println("Угадал! Это была великолепная игра!")
+            return
+        } else if (counter == 1) {
+            println("Попытки закончились, спасибо за игру! Было загадано число $number.")
+        } else {
+            println("У тебя осталось ${--counter} попытки.")
+        }
+    }
+}
