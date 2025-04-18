79c04788b8fd57cc7b9b4d718560edf00cfaad1b
Anton <new_perspective@mail.ru>
2025-04-18 13:27:56 +0300

    KS-7-1 password write to variable

--- src/main/kotlin/Lesson_7/lesson7_task1.kt
+++ src/main/kotlin/Lesson_7/lesson7_task1.kt
@@ -4,10 +4,17 @@ const val LETTERS_FROM: Char = 'a'
 const val LETTERS_TO: Char = 'z'
 const val NUMBERS_FROM: Int = 0
 const val NUMBERS_TO: Int = 9
+const val QTY_OF_EACH_ELEMENT: Int = 3
 
 fun main() {
-    for (i in 1..3) {
-        print((LETTERS_FROM..LETTERS_TO).random())
-        print((NUMBERS_FROM..NUMBERS_TO).random())
+
+    var passwordGenerated: String = ""
+
+    repeat(QTY_OF_EACH_ELEMENT) {
+        passwordGenerated += (LETTERS_FROM..LETTERS_TO).random()
+        passwordGenerated += (NUMBERS_FROM..NUMBERS_TO).random()
     }
+
+    println(passwordGenerated)
+
 }
