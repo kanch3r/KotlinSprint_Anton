61af28126c881bef4783be17a01f9d00d0a03aad
Anton <new_perspective@mail.ru>
2025-04-23 15:07:11 +0300

    KS-7-5 correct variables and type

--- src/main/kotlin/Lesson_7/lesson7_task5.kt
+++ src/main/kotlin/Lesson_7/lesson7_task5.kt
@@ -4,8 +4,8 @@ const val SMALL_LETTERS_FROM: Char = 'a'
 const val SMALL_LETTERS_TO: Char = 'z'
 const val BIG_LETTERS_FROM: Char = 'A'
 const val BIG_LETTERS_TO: Char = 'Z'
-const val NUMBERS_START: Int = 0
-const val NUMBERS_END: Int = 9
+const val NUMBERS_START: Char = '0'
+const val NUMBERS_END: Char = '9'
 const val BASE_QTY_OF_SYMBOLS: Int = 3
 
 fun main() {
@@ -20,12 +20,9 @@ fun main() {
     passwordGenerated += (NUMBERS_START..NUMBERS_END).random()
 
     repeat(userInputPasswordLength - BASE_QTY_OF_SYMBOLS) {
-        val randomList = listOf(
-            (SMALL_LETTERS_FROM..SMALL_LETTERS_TO).toList(),
-            (BIG_LETTERS_FROM..BIG_LETTERS_TO).toList(),
-            (NUMBERS_START..NUMBERS_END).toList()
-        ).random()
-        val randomElement = randomList.random()
+        val allChars = (SMALL_LETTERS_FROM..SMALL_LETTERS_TO) + (BIG_LETTERS_FROM..BIG_LETTERS_TO) +
+                (NUMBERS_START..NUMBERS_END).random()
+        val randomElement = allChars.random()
         passwordGenerated += randomElement
     }
 
