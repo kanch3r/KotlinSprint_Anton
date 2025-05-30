a1db81c7bd226ce47c6ed7cfd2b5e33e6d36b3f8
Anton <new_perspective@mail.ru>
2025-05-30 23:11:20 +0300

    KS-13-3 user list creation and display

---
+++ src/main/kotlin/Lesson_13/lesson13_task3.kt
@@ -0,0 +1,22 @@
+package org.example.Lesson_13
+
+class DirectoryThird(val name: String, val number: Long, val company: String? = null) {
+
+    fun printData() {
+        println("Имя:$name \nНомер:$number \nКомпания:${company ?: "не указано"}")
+    }
+}
+
+fun main() {
+
+    val userList = listOf(
+        DirectoryThird("Garry", 123456789),
+        DirectoryThird("Ron", 123456789, "Hogwarts"),
+        DirectoryThird("Germiona", 123456789),
+        DirectoryThird("Draco", 123456789, "Slytherin"),
+        DirectoryThird("Sirius", 123456789, "null"),
+    )
+
+    userList.forEach { println(it.company) }
+
+}
