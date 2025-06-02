fc73b8943d7b406efa3a36e0e687e1a8e4514f97
Anton <new_perspective@mail.ru>
2025-06-02 12:23:03 +0300

    KS-13-3 apply unique company

--- src/main/kotlin/Lesson_13/lesson13_task3.kt
+++ src/main/kotlin/Lesson_13/lesson13_task3.kt
@@ -1,22 +1,18 @@
 package org.example.Lesson_13
 
-class DirectoryThird(val name: String, val number: Long, val company: String? = null) {
-
-    fun printData() {
-        println("Имя:$name \nНомер:$number \nКомпания:${company ?: "не указано"}")
-    }
-}
+class ContactColleague(val name: String, val number: Long, val company: String? = null)
 
 fun main() {
 
     val userList = listOf(
-        DirectoryThird("Garry", 123456789),
-        DirectoryThird("Ron", 123456789, "Hogwarts"),
-        DirectoryThird("Germiona", 123456789),
-        DirectoryThird("Draco", 123456789, "Slytherin"),
-        DirectoryThird("Sirius", 123456789, "null"),
+        ContactColleague("Garry", 123456789),
+        ContactColleague("Ron", 123456789, "Hogwarts"),
+        ContactColleague("Germiona", 123456789, "Hogwarts"),
+        ContactColleague("Draco", 123456789, "Slytherin"),
+        ContactColleague("Sirius", 123456789, "null"),
     )
 
-    userList.forEach { if (it.company != null) println(it.company)  }
+    val uniqueCompany = userList.mapNotNull { it.company }.toSet()
+    println(uniqueCompany.joinToString())
 
 }
