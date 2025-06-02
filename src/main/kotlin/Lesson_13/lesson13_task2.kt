5b1a9f49a07b0e0cd9adf766fbde378c2d587bb4
Anton <new_perspective@mail.ru>
2025-06-02 11:42:43 +0300

    KS-13-2 class name changed,non-used object deleted

--- src/main/kotlin/Lesson_13/lesson13_task2.kt
+++ src/main/kotlin/Lesson_13/lesson13_task2.kt
@@ -1,6 +1,6 @@
 package org.example.Lesson_13
 
-class DirectorySecond(val name: String, val number: Long, val company: String? = null) {
+class ContactStudent(val name: String, val number: Long, val company: String? = null) {
 
     fun printData() {
         println("Имя:$name \nНомер:$number \nКомпания:${company ?: "не указано"}")
@@ -9,9 +9,8 @@ class DirectorySecond(val name: String, val number: Long, val company: String? =
 
 fun main() {
 
-    val user1 = DirectorySecond("Garry", 123456789)
-    val user2 = DirectorySecond("Ron", 123456789, "Hogwarts")
-    val user3 = DirectorySecond("Germiona", 123456789)
+    val user1 = ContactStudent("Garry", 123456789)
+    val user2 = ContactStudent("Ron", 123456789, "Hogwarts")
 
     user1.printData()
     user2.printData()
