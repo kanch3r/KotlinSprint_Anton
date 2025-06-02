aa145ef202dda0d2c64b393873114d1655ffb5a7
Anton <new_perspective@mail.ru>
2025-06-02 11:29:48 +0300

    KS-13-1 class name changed

--- src/main/kotlin/Lesson_13/lesson13_task1.kt
+++ src/main/kotlin/Lesson_13/lesson13_task1.kt
@@ -1,15 +1,3 @@
 package org.example.Lesson_13
 
-class Directory(val name: String, val number: Long, val company: String? = null)
-
-fun main() {
-
-    val user1 = Directory("Garry", 123456789)
-    val user2 = Directory("Ron", 123456789, "Hogwarts")
-    val user3 = Directory("Germiona", 123456789)
-
-    println(user1.name)
-    println(user2.company)
-    println(user3.company)
-
-}
+class Contact(val name: String, val number: Long, val company: String?)
