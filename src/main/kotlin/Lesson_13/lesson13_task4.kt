fb14fcedb603730eb1f213d42c9e801b15925ab2
Anton <new_perspective@mail.ru>
2025-06-03 12:47:31 +0300

    KS-13-4 user list outside the class

--- src/main/kotlin/Lesson_13/lesson13_task4.kt
+++ src/main/kotlin/Lesson_13/lesson13_task4.kt
@@ -2,26 +2,26 @@ package org.example.Lesson_13
 
 class ContactEmployee(val name: String, val number: Long?, val company: String? = null) {
 
-    val listOfContacts: MutableList<ContactEmployee> = mutableListOf()
-
-    fun addToList() = listOfContacts.add(ContactEmployee(name, number, company))
-
     fun printData() = println("Имя:$name \nНомер:$number \nКомпания:${company ?: "не указано"}")
 }
 
 fun main() {
 
+    val userList: MutableList<ContactEmployee> = mutableListOf()
+
     println("Введите имя контакта:")
     val contactName: String = readln()
 
     println("Введите номер контакта:")
-    val contactNumber: Long? = readln().toLongOrNull() ?: return println("Невверный формат!")
+    val contactNumber: Long? = readln().toLongOrNull() ?: return println("Неверный формат!")
 
     println("Введите организацию:")
-    val contactCompany: String? = readln()
+    val contactCompany: String? = readlnOrNull()?.ifEmpty { null }
 
     val newUser = ContactEmployee(contactName, contactNumber, contactCompany)
-    newUser.addToList()
-    newUser.printData()
+
+    userList.add(newUser)
+
+    userList.forEach { it.printData() }
 
 }
