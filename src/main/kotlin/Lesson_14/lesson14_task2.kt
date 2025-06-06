a387cf5cb349a367ff2ae636002162ad4105585d
Anton <new_perspective@mail.ru>
2025-06-06 11:20:30 +0300

    KS-14-2 classes corrected, method updated

--- src/main/kotlin/Lesson_14/lesson14_task2.kt
+++ src/main/kotlin/Lesson_14/lesson14_task2.kt
@@ -1,21 +1,30 @@
 package org.example.Lesson_14
 
 open class LinerBoat(
-    val name: String,
-    val speed: Int,
-    val capacity: Int,
-    val icebreaker: Boolean = false,
+    open val name: String = "Motherboard",
+    open val speed: Int = 500,
+    open val capacity: Int = 1000,
+    open val icebreaker: Boolean = false,
 ) {
+    open fun displayShipInfo() {
+        println(
+            "Имя корабля: $name," +
+                    "Скорость корабля: $speed, " +
+                    "Вместимость: $capacity, " +
+                    "Ледокол?: ${if (icebreaker) "да" else "нет"}"
+        )
+    }
+
     open fun startLoad() {
         println("${name}: Выдвинуть горизонтальный трап со шкафута")
     }
 }
 
 class CargoBoat(
-    name: String,
-    speed: Int,
-    capacity: Int,
-) : LinerBoat(name, speed, capacity) {
+    override val name: String,
+    override val speed: Int,
+    override val capacity: Int,
+) : LinerBoat() {
 
     override fun startLoad() {
         println("${name}: Активировать погрузочный кран")
@@ -24,10 +33,11 @@ class CargoBoat(
 }
 
 class IcebreakerBoat(
-    name: String,
-    speed: Int,
-    capacity: Int,
-) : LinerBoat(name, speed, capacity, icebreaker = true) {
+    override val name: String,
+    override val speed: Int,
+    override val capacity: Int,
+    override val icebreaker: Boolean
+) : LinerBoat() {
 
     override fun startLoad() {
         println("${name}: Открыть ворота со стороны кормы")
@@ -35,12 +45,16 @@ class IcebreakerBoat(
 }
 
 fun main() {
-    val linerShip = LinerBoat("MotherBoard", 500, 1000)
+    val linerShip = LinerBoat()
     val cargoShip = CargoBoat("Heavy", 300, 800)
-    val icebreakerShip = IcebreakerBoat("Iceboat", 200, 500)
+    val icebreakerShip = IcebreakerBoat("Iceboat", 200, 500, true)
 
     linerShip.startLoad()
     cargoShip.startLoad()
     icebreakerShip.startLoad()
+    println()
+    linerShip.displayShipInfo()
+    cargoShip.displayShipInfo()
+    icebreakerShip.displayShipInfo()
 
 }
