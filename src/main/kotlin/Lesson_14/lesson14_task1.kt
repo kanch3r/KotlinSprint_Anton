e290ded6519f0728c1a0e3e916cdf1ac2498e105
Anton <new_perspective@mail.ru>
2025-06-06 18:43:38 +0300

    KS-14-1 inheritance corrected

--- src/main/kotlin/Lesson_14/lesson14_task1.kt
+++ src/main/kotlin/Lesson_14/lesson14_task1.kt
@@ -1,28 +1,32 @@
 package org.example.Lesson_14
 
 open class LinerShip(
-    open val name: String = "MotherBoard",
+    open val name: String = "Motherboard",
     open val speed: Int = 500,
-    open val capacity: Int = 1000,
+    open val passengerCapacity: Int = 1000,
     open val icebreaker: Boolean = false,
 )
 
 class CargoShip(
     override val name: String,
-    override val speed: Int,
-    override val capacity: Int,
-) : LinerShip()
+    val cargoCapacity: Int = 1000,
+) : LinerShip(
+    name = name,
+    speed = 100,
+    passengerCapacity = 10,
+)
 
 class IcebreakerShip(
     override val name: String,
-    override val speed: Int,
-    override val capacity: Int,
-    override val icebreaker: Boolean
-) : LinerShip()
+) : LinerShip(
+    name = name,
+    speed = 50,
+    passengerCapacity = 10,
+    icebreaker = true,
+)
 
 fun main() {
     val linerShip = LinerShip()
-    val cargoShip = CargoShip("Heavy", 300, 800)
-    val icebreakerShip = IcebreakerShip("Iceboat", 200, 500, true)
-
+    val cargoShip = CargoShip("Heavy")
+    val icebreakerShip = IcebreakerShip("Iceboat")
 }
