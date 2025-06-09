da16e12e75871bdf8ce65b30e08138fa914ebd6b
Anton <new_perspective@mail.ru>
2025-06-09 16:45:43 +0300

    KS-14-3 made const val, original list for figures

--- src/main/kotlin/Lesson_14/lesson14_task4.kt
+++ src/main/kotlin/Lesson_14/lesson14_task4.kt
@@ -1,23 +1,28 @@
 package org.example.Lesson_14
 
-abstract class CelestialBody(
+open class CelestialBody(
     open val name: String,
     open val size: Int,
-    open val hasLanded: Boolean,
+    open val isPossibleForLanding: Boolean,
 )
 
 class Planet(
     override val name: String,
     override val size: Int,
-    override val hasLanded: Boolean = false,
-    val listOfSatellites: List<Satellite> = emptyList(),
-) : CelestialBody(name, size, hasLanded)
+    override val isPossibleForLanding: Boolean = false,
+    val satellites: List<Satellite> = emptyList(),
+) : CelestialBody(name, size, isPossibleForLanding) {
+
+    fun printInfo() {
+        println("Название планеты: $name. \nСпутники: ${satellites.joinToString { it.name }}")
+    }
+}
 
 class Satellite(
     override val name: String,
     override val size: Int,
-    override val hasLanded: Boolean = false,
-) : CelestialBody(name, size, hasLanded)
+    override val isPossibleForLanding: Boolean = false,
+) : CelestialBody(name, size, isPossibleForLanding)
 
 fun main() {
 
@@ -25,7 +30,5 @@ fun main() {
     val satellite2 = Satellite("Деймос", 5)
     val planet1 = Planet("Марс", 100, true, listOf(satellite1, satellite2))
 
-    println("Название планеты: ${planet1.name}")
-    println("Спутники планеты: ${planet1.listOfSatellites.joinToString { it.name }}")
-
+    planet1.printInfo()
 }
