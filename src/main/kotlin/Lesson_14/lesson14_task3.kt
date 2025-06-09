4d5f6238c2a63d8953291b3cf20f53397f6a84d5
Anton <new_perspective@mail.ru>
2025-06-09 16:33:00 +0300

    KS-14-3 made const val, original list for figures

--- src/main/kotlin/Lesson_14/lesson14_task3.kt
+++ src/main/kotlin/Lesson_14/lesson14_task3.kt
@@ -2,13 +2,16 @@ package org.example.Lesson_14
 
 import kotlin.math.PI
 
-abstract class Figure(val color: String) {
+const val COLOR_BLACK = "Black"
+const val COLOR_WHITE = "White"
+
+abstract class Figure(open val color: String) {
     abstract fun calculateSquare(): Int
     abstract fun calculatePerimeter(): Int
 }
 
 class Circle(
-    color: String,
+    override val color: String,
     val radius: Int
 ) : Figure(color) {
     override fun calculateSquare(): Int = (PI * radius * radius).toInt()
@@ -16,7 +19,7 @@ class Circle(
 }
 
 class Rectangle(
-    color: String,
+    override val color: String,
     val height: Int,
     val width: Int
 ) : Figure(color) {
@@ -26,20 +29,18 @@ class Rectangle(
 
 fun main() {
 
-    val listOfFigures: MutableList<Figure> = mutableListOf()
-
-    val circle1 = Circle("Белый", 5)
-    val circle2 = Circle("Белый", 4)
-    val rectangle1 = Rectangle("Черный", 5, 6)
-    val rectangle2 = Rectangle("Черный", 4, 6)
-
-    listOfFigures.addAll(listOf(circle1, circle2, rectangle1, rectangle2))
+    val listOfFigures = listOf(
+        Circle(COLOR_BLACK, 5),
+        Circle(COLOR_WHITE, 4),
+        Rectangle(COLOR_BLACK, 5, 6),
+        Rectangle(COLOR_WHITE, 4, 6)
+    )
 
     val totalSumOfPerimetersOfBlackFigures = listOfFigures
-        .filter { it.color == "Черный" }
+        .filter { it.color == "Black" }
         .sumOf { it.calculatePerimeter() }
     val totalSumOfSquaresOfWhiteFigures = listOfFigures
-        .filter { it.color == "Белый" }
+        .filter { it.color == "White" }
         .sumOf { it.calculateSquare() }
 
     println("Сумма периметров всех черных фигур: $totalSumOfPerimetersOfBlackFigures")
