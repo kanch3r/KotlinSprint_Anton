40158eda6cfee56c2ce4dcb459ec0d1129d6b4b3
Anton <new_perspective@mail.ru>
2025-06-30 15:23:51 +0300

    KS-15-5 remove abstract class, correct logic

--- src/main/kotlin/Lesson_15/lesson15_task5.kt
+++ src/main/kotlin/Lesson_15/lesson15_task5.kt
@@ -1,23 +1,18 @@
 package org.example.Lesson_15
 
-abstract class Car: MoveCar {
-    abstract override val name: String
-    abstract val passengerCapacity: Int
-}
-
 class PassengerCar(
     override val name: String,
     override val passengerCapacity: Int,
     override var currentPassengers: Int = 0,
-) : Car(), MovePassenger
+) : MovePassenger, MoveCar
 
 class CargoCar(
     override val name: String,
     override val passengerCapacity: Int,
     override val cargoCapacity: Int,
-    override var currentPassengers: Int = 1,
+    override var currentPassengers: Int = 0,
     override var currentCargo: Int = 0
-) : Car(), MovePassenger, MoveCargo
+) : MovePassenger, MoveCar, MoveCargo
 
 interface MoveCar {
     val name: String
@@ -37,7 +32,7 @@ interface MovePassenger {
     fun addPassenger() {
         if (currentPassengers < passengerCapacity) {
             currentPassengers++
-            println("Добавлен пассажир в $name. Всего $currentPassengers пассажиров.")
+            println("Добавлен пассажир в $name. Всего $currentPassengers пассажиров из $passengerCapacity.")
         } else {
             println("$name: достигнута максимальная вместимость")
         }
@@ -46,7 +41,7 @@ interface MovePassenger {
     fun removePassenger() {
         if (currentPassengers > 0) {
             currentPassengers--
-            println("Пассажир высажен. Осталось $currentPassengers пассажиров.")
+            println("Пассажир высажен из $name. Осталось $currentPassengers пассажиров из $passengerCapacity.")
         } else {
             println("$name пуст.")
         }
@@ -60,7 +55,7 @@ interface MoveCargo {
     fun addCargo() {
         if (currentCargo < cargoCapacity) {
             currentCargo += 1000
-            println("Загружено 1000 киллограм в $name. Всего $currentCargo груза.")
+            println("Загружено 1000 киллограм в $name. Всего $currentCargo груза из $cargoCapacity.")
         } else {
             println("$name уже заполнен грузом.")
         }
@@ -69,7 +64,7 @@ interface MoveCargo {
     fun removeCargo() {
         if (currentCargo > 0) {
             currentCargo -= 1000
-            println("Выгружено 1000 килограмм. Осталось $currentCargo груза.")
+            println("Выгружено 1000 килограмм. Осталось $currentCargo груза из $cargoCapacity.")
         } else {
             println("$name: весь груз выгружен.")
         }
@@ -87,23 +82,23 @@ fun main() {
     passengerCarMain.addPassenger()
     passengerCarAdditional.addPassenger()
     passengerCarAdditional.addPassenger()
-    passengerCarAdditional.addPassenger()
+    cargoCar.addPassenger()
     cargoCar.addCargo()
     cargoCar.addCargo()
-
+    println()
     passengerCarMain.startDrive()
     passengerCarAdditional.startDrive()
     cargoCar.startDrive()
     passengerCarMain.stopDrive()
     passengerCarAdditional.stopDrive()
     cargoCar.stopDrive()
-
+    println()
     passengerCarMain.removePassenger()
     passengerCarMain.removePassenger()
     passengerCarMain.removePassenger()
     passengerCarAdditional.removePassenger()
     passengerCarAdditional.removePassenger()
-    passengerCarAdditional.removePassenger()
+    cargoCar.removePassenger()
     cargoCar.removeCargo()
     cargoCar.removeCargo()
 
