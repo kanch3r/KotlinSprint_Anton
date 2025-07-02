ca2ea25157078848b1349260be7639b34509366e
Anton <new_perspective@mail.ru>
2025-07-02 16:20:48 +0300

    KS-17-1 base setter and getter

--- src/main/kotlin/Lesson_17/lesson17_task1.kt
+++ src/main/kotlin/Lesson_17/lesson17_task1.kt
@@ -1,25 +1,18 @@
 package org.example.Lesson_17
 
-class Quiz (question: String, answer: String) {
+class Quiz(question: String, answer: String) {
     val questionQuiz = question
-        get() {
-            println(field)
-            return field
-        }
+        get() = field
     var answerQuiz = answer
-        get() {
-            println("Правильный ответ: $field")
-            return field
-        }
+        get() = field
         set(value) {
-            if (value == field) println("Верно!") else println("Ответ неверный.")
+            field = value
         }
-
 }
 
 fun main() {
     val quiz = Quiz("Столица Тайланда?", "Банкок")
-    quiz.questionQuiz
+    println(quiz.questionQuiz)
     quiz.answerQuiz = "Пхукет"
-    quiz.answerQuiz
+    println(quiz.answerQuiz)
 }
