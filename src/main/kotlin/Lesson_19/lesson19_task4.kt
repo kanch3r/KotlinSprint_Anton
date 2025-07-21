a415aef7071680bab7aefa301d5521d26b4f7b35
Anton <new_perspective@mail.ru>
2025-07-21 22:01:51 +0300

    KS-19-4 tank with different bullets

---
+++ src/main/kotlin/Lesson_19/lesson19_task4.kt
@@ -0,0 +1,38 @@
+package org.example.Lesson_19
+
+enum class Bullet(val bulletName: String, val bulletDamage: Int) {
+    BLUE("Blue bullets", 5),
+    GREEN("Green bullets", 10),
+    RED("Red bullets", 20),
+}
+
+class Tank(
+    val name: String,
+    var ammoName: String = "No ammo",
+    var ammoDamage: Int = 0,
+    ) {
+
+    fun takeBullet(bullet: Bullet) {
+        ammoName = bullet.bulletName
+        ammoDamage = bullet.bulletDamage
+    }
+
+    fun openFire() {
+        if (ammoName != "No ammo" && ammoDamage != 0) {
+            println("$name fire with $ammoName and deals $ammoDamage damage.")
+        } else {
+            println("$name out of ammo. Can't take a shot.")
+        }
+    }
+}
+
+fun main() {
+    val tank1 = Tank("T-34")
+    tank1.openFire()
+    tank1.takeBullet(Bullet.BLUE)
+    tank1.openFire()
+    tank1.takeBullet(Bullet.GREEN)
+    tank1.openFire()
+    tank1.takeBullet(Bullet.RED)
+    tank1.openFire()
+}
