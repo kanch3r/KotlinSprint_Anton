f34ef209ab49d77e12f4d47cdeb112c83561dbd5
Anton <new_perspective@mail.ru>
2025-07-21 15:09:15 +0300

    KS-19-1 enum instances had named

--- src/main/kotlin/Lesson_19/lesson19_task1.kt
+++ src/main/kotlin/Lesson_19/lesson19_task1.kt
@@ -1,16 +1,15 @@
 package org.example.Lesson_19
 
-enum class Fish {
-    GUPPY,
-    ANGELFISH,
-    GOLDFISH,
-    SIAMESE_FIGHTING_FISH,
+enum class Fish(val nameFish: String) {
+    GUPPY("Гуппи"),
+    ANGELFISH("Скалярия"),
+    GOLDFISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
 
 fun main() {
 
-    val fishForAquarium = Fish.entries.joinToString { it.name }
+    val fishForAquarium = Fish.entries.joinToString { it.nameFish }
 
     println("Доступные рыбки для добавления в аквариум: $fishForAquarium")
-
 }
