8cbec7495831846c6d850619ce6e88b3085f4e96
Anton <new_perspective@mail.ru>
2025-07-30 21:53:52 +0300

    KS-21-1 enum replaced by val

--- src/main/kotlin/Lesson_21/lesson21_task1.kt
+++ src/main/kotlin/Lesson_21/lesson21_task1.kt
@@ -1,10 +1,8 @@
 package org.example.Lesson_21
 
-enum class VowelLetters(val letters: List<Char>) {
-    VOWEL(listOf('a', 'e', 'i', 'o', 'u'))
-}
+val vowelLetters: List<Char> = listOf('a', 'e', 'i', 'o', 'u')
 
-fun String.vowelCount() = this.count { it in VowelLetters.VOWEL.letters }
+fun String.vowelCount() = this.lowercase().count { it in vowelLetters }
 
 fun main() {
     val word = "hello"
