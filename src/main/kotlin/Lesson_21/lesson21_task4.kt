68a97f70ffb4a57169a93a5a94e02530e0076edd
Anton <new_perspective@mail.ru>
2025-07-30 22:45:05 +0300

    KS-21-4 rewrite fun

--- src/main/kotlin/Lesson_21/lesson21_task4.kt
+++ src/main/kotlin/Lesson_21/lesson21_task4.kt
@@ -2,7 +2,10 @@ package org.example.Lesson_21
 
 import java.io.File
 
-fun File.writeWordToFile(word: String) = this.writeText(word.lowercase())
+fun File.writeWordToFile(word: String) {
+    val currentText = if (this.exists()) this.readText() else ""
+    this.writeText("${word.lowercase()} + ${currentText.lowercase()}")
+}
 
 fun main() {
     val newFile = File("Файл, куда будет записываться новое слово.txt")
